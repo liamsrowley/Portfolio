b9092cfc8b0d6eafb39a18d221011a74beedcc6d
Liam Rowley <liam.rowley@hotmail.co.uk>
2020-01-23 10:39:07 +0000

    fix z index on mobile nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,11 @@
 module.exports = {
+
+  siteMetadata: {
+    title: 'Liam Rowley | Portfolio',
+    description: 'Portfolio of Liam Rowley, front-end web developer specialising in React development.',
+    author: 'Liam Rowley'
+  },
+
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-transformer-remark',
